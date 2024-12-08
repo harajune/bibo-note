4d212b3b62d6730807b44c057c89666b4b0f3593
Jun Harada <jun.harada@txpmedical.com>
2024-12-08 22:52:05 +0900

    Refactor Sidebar component to accept dynamic menu sections and add DashboardIcon component for improved navigation

--- app/viewer/viewer.tsx
+++ app/viewer/viewer.tsx
@@ -1,13 +1,36 @@
 import Counter from "../islands/counter";
 import { Sidebar } from "../global/$sidebar";
+import { DashboardIcon } from "../global/icons";
 interface ViewerProps {
   name: string;
 }
 
 export function Viewer({ name }: ViewerProps) {
+  const menuSections = [
+    {
+      children: [{
+        name: "Dashboard",
+        href: "/dashboard",
+        current: true,
+        icon: DashboardIcon()
+      }]
+    },
+    {
+      header: {name: "Your Teams"},
+      children: [
+        {
+          name: "Projects",
+          href: "/projects",
+          current: false,
+          icon: DashboardIcon()
+        }
+      ]
+    }
+  ];
+
   return (
     <div>
-      <Sidebar />
+      <Sidebar children={menuSections} />
 
       <main class="py-10 lg:pl-72">
         <div class="px-4 sm:px-6 lg:px-8">
