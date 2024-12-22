5fb2c73b730254785753f3ea32a597ae204d0297
Jun Harada <jun.harada@txpmedical.com>
2024-12-22 15:04:33 +0900

    Include clobal.css in the build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import build from '@hono/vite-build/cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
+import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [client()],
+      build: {
+        rollupOptions: {
+          input: ['/app/global.css']
+        }
+      }
+    }
+  }
+  return {
+    plugins: [honox({ devServer: { adapter } }), build()]
+  }
 })
