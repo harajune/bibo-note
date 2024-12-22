233d64eca7001c9613253e3f1d7fda98350d3adc
Jun Harada <jun.harada@txpmedical.com>
2024-12-22 21:16:02 +0900

    simplify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import build from '@hono/vite-build/cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
-import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      plugins: [client()],
-      build: {
-        rollupOptions: {
-          input: ['/app/global.css']
-        }
-      }
-    }
-  }
   return {
-    plugins: [honox({ devServer: { adapter } }), build()]
+    plugins: [
+      honox({
+        client: {
+          input: ['/app/global.css']
+        },
+        devServer: { adapter } 
+    }), 
+    build()]
   }
 })
