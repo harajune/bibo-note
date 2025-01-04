5d712c095c89a9abe92c67f2319503a67089c144
Jun Harada <jun.harada@txpmedical.com>
2025-01-05 01:41:03 +0900

    Update project configuration for test coverage
    - Enhance .gitignore to exclude coverage directory.
    - Add new test command "test:coverage" in package.json for running tests with coverage.
    - Configure Vitest to use V8 coverage provider with multiple report formats (text, json, html).
    - Update yarn.lock to include new dependencies related to coverage reporting.
    
    These changes improve the testing setup by enabling coverage analysis, ensuring better code quality and maintainability.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config'
 //TODO: don't forget to merge with vite.config.ts
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   }
 });
