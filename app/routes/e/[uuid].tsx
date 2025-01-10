c4b600016c7bbe5bf93ec388df402318be1fce78
Jun Harada <jun.harada@txpmedical.com>
2025-01-10 21:52:05 +0900

    feat: refactor routes to support async loading of wiki data
    - Updated the GET and POST routes in [uuid].tsx to use async/await for loading wiki data, improving performance and error handling.
    - Ensured that the wiki data is fetched asynchronously in both the editor and viewer components, enhancing the responsiveness of the application.

--- app/routes/e/[uuid].tsx
+++ app/routes/e/[uuid].tsx
@@ -3,10 +3,10 @@ import { Editor } from "../../wiki/screens/editor";
 import { WikiModel } from "../../wiki/models/wiki_model";
 import { createImmutable } from "../../libs/immutable/immutable";
 
-export default createRoute((c) => {
+export default createRoute(async (c) => {
   const uuid = c.req.param("uuid");
   const wikiModel = new WikiModel();
-  const wikiData = wikiModel.load(uuid);
+  const wikiData = await wikiModel.load(uuid);
   return c.render(<Editor wikiData={wikiData} />,
      { title: wikiData.title });
 });
@@ -16,13 +16,13 @@ export const POST = createRoute(async (c) => {
   const wikiModel = new WikiModel();
 
   try {
-    const wikiData = createImmutable(wikiModel.load(uuid));
+    const wikiData = createImmutable(await wikiModel.load(uuid));
     const data = await c.req.formData();  
     const updatedWikiData = wikiData.copyWith({
       title: data.get("title") as string,
       content: data.get("content") as string
     });
-    wikiModel.save(updatedWikiData);
+    await wikiModel.save(updatedWikiData);
     return c.redirect(`/v/${uuid}`);
   } catch (e) {
     return c.notFound();
--- app/routes/v/[uuid].tsx
+++ app/routes/v/[uuid].tsx
@@ -2,10 +2,10 @@ import { createRoute } from "honox/factory";
 import { Viewer } from "../../wiki/screens/viewer";
 import { WikiModel } from "../../wiki/models/wiki_model";
 
-export default createRoute((c) => {
+export default createRoute(async (c) => {
   const uuid = c.req.param("uuid");
   const wikiModel = new WikiModel();
-  const wikiData = wikiModel.load(uuid);
+  const wikiData = await wikiModel.load(uuid);
   
   return c.render(
     <Viewer wikiData={wikiData} />,
