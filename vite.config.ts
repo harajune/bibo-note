9986e946776dd61208afdd185bde94aa16bfcd46
harajune <harajune@gijutsuya.jp>
2025-05-21 12:23:57 +0900

    refactor: remove unused adapter import from vite.config.ts
    - Eliminated the unused 'adapter' import from the Vite configuration file to clean up the code.
    - This change simplifies the configuration and improves maintainability.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import build from '@hono/vite-build/node'
-import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
 
@@ -12,8 +11,7 @@ export default defineConfig(({ mode }) => {
       honox({
         client: {
           input: ['/app/global.css'],
-        },
-        devServer: { adapter } 
+        }
       }), 
       build({
         outputDir: 'dist/worker',
