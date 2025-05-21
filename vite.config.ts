0854627c94b6afd9e7410b977d2ad177d183995e
Jun Harada <harajune@gijutsuya.jp>
2025-05-21 12:15:06 +0900

    fix dev server by lazy loading aws sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import build from '@hono/vite-build/aws-lambda'
+import build from '@hono/vite-build/node'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
