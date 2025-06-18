17e8a47ac8f337dbee37434616298dde97a47a7d
harajune <harajune@gijutsuya.jp>
2025-06-18 12:04:36 +0900

    Enhance WebAssembly binary handling in OGP Vite configuration
    - Updated the transformation logic to handle both resvg.wasm and yoga.wasm files, returning base64 strings in different formats for improved compatibility.
    - Introduced separate functions for creating base64 data URIs and byte buffers, streamlining the handling of WebAssembly binaries.

--- packages/ogp/vite.config.ts
+++ packages/ogp/vite.config.ts
@@ -26,22 +26,40 @@ export default defineConfig({
     {
       name: 'vite-plugin-base64',
       async transform(source, id) {
-          if (!id.match(/.*\.wasm(?:\?.*)?$/)) return
+        if (id.match(/.*resvg\.wasm(?:\?.*)?$/)) {
           console.log(id)
           const filename = id.replace(/\?.*$/, '')
           console.log(filename)
           const file = readFileSync(filename)
           const base64 = file.toString('base64')
-          return createCode(base64)
+          return createCodeBase64(base64)
+
+        } else if (id.match(/.*yoga\.wasm(?:\?.*)?$/)) {
+          console.log(id)
+          const filename = id.replace(/\?.*$/, '')
+          console.log(filename)
+          const file = readFileSync(filename)
+          const base64 = file.toString('base64')
+          return createCodeBytes(base64)
+        }
+        
+        return null
       },
-    },
-    wasm()
+    }
   ],
 })
 
-function createCode(base64: string) {
+function createCodeBase64(base64: string) {
   return `
   const base64String = "data:application/wasm;base64,${base64}";
   export default base64String;
+`
+}
+
+function createCodeBytes(base64: string) {
+  return `
+  const base64String = "${base64}";
+  const buffer = Buffer.from(base64String, 'base64');
+  export default buffer;
 `
 }
